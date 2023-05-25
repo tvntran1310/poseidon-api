cfe7d9256c7b3156c18ac4c652c8f8e5fb04a7e7
tvntran1310 <truongvntran@gmail.com>
2023-05-25 10:22:38 +0700

    connect to mongodb

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 const app = express();
+
+// connect mongo
+const MONGO_HOST = process.env.MONGO_HOST;
+const MONGO_PORT = process.env.MONGO_PORT;
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`)
+  .then(() => console.log('success connect') )
+  .catch(err => console.error(err));
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const port = 2000;
+const port = process.env.SERVER_PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });

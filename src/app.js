e82f3c1b904a256312a1f16b12b05e0e695b911c
tvntran1310 <truongvntran@gmail.com>
2023-05-25 16:01:17 +0700

    feat: apply eslint

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
-dotenv.config()
+
+dotenv.config();
 
 const app = express();
 
 // connect mongo
-const MONGO_HOST = process.env.MONGO_HOST;
-const MONGO_PORT = process.env.MONGO_PORT;
-const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
-mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`)
-  .then(() => console.log('success connect') )
-  .catch(err => console.error(err));
+const { MONGO_HOST, MONGO_PORT, MONGO_DB_NAME } = process.env;
+mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`);
 
 app.use(express.json());
 
@@ -20,6 +17,4 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.SERVER_PORT || 3000;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+app.listen(port);
